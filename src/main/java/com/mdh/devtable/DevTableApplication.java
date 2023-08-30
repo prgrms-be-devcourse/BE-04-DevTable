40799853f7429168dd7bbc44afbdc38a4e4375fd
Bombo <bombo96@chungwoon.ac.kr>
2023-08-30 18:08:51 +0900

    MDH-2 feat : REST DOCS 추가

--- src/main/java/com/mdh/devtable/DevTableApplication.java
+++ src/main/java/com/mdh/devtable/DevTableApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevtableApplication {
+public class DevTableApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevtableApplication.class, args);
+		SpringApplication.run(DevTableApplication.class, args);
 	}
 
 }
