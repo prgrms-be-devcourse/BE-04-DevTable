ffe2cce9f4f437ad23b908a7d44ed583367d8371
Bombo <bombo96@chungwoon.ac.kr>
2023-08-30 16:26:46 +0900

    MDH-1 init : 프로젝트 초기 세팅

---
+++ src/main/java/com/mdh/devtable/DevtableApplication.java
@@ -0,0 +1,13 @@
+package com.mdh.devtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtableApplication.class, args);
+	}
+
+}
