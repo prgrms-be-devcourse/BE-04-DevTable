4668412946d00aadf5a22e6d4061312e37d71584
joyfulviper <kimsk3113@naver.com>
2023-09-02 00:33:56 +0900

    [MDH-6] feat : User Entity 추가

---
+++ src/main/java/com/mdh/devtable/user/domain/User.java
@@ -0,0 +1,41 @@
+package com.mdh.devtable.user.domain;
+
+import com.mdh.devtable.global.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", length = 63, nullable = false)
+    private String email;
+
+    @Column(name = "role", length = 15, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "password", length = 20, nullable = false)
+    private String password;
+
+    @Builder
+    public User(Long id, String email, Role role, String password) {
+        this.id = id;
+        this.email = email;
+        this.role = role;
+        this.password = password;
+    }
+
+    public void confirmPassword(String password) {
+        if (!password.equals(this.password)) {
+            throw new IllegalArgumentException("Password and confirm password do not match");
+        }
+    }
+}
---
+++ src/test/java/com/mdh/devtable/user/domain/UserTest.java
@@ -0,0 +1,47 @@
+package com.mdh.devtable.user.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+    @Test
+    @DisplayName("비밀번호를 정상적으로 확인한다.")
+    void testConfirmPassword_MatchingPasswords() {
+        // given
+        User user = User.builder()
+                .id(1L)
+                .email("test@example.com")
+                .role(Role.GUEST)
+                .password("password123")
+                .build();
+
+        String confirmPassword = "password123";
+
+        // when & then
+        // No exception should be thrown
+        assertDoesNotThrow(() -> user.confirmPassword(confirmPassword));
+
+        // then
+        // If no exception is thrown, the test passes
+    }
+
+    @Test
+    @DisplayName("비밀번호 확인이 알맞지 않으면 예외를 던진다")
+    void testConfirmPassword_NonMatchingPasswords() {
+        // given
+        User user = User.builder()
+                .id(1L)
+                .email("test@example.com")
+                .role(Role.GUEST)
+                .password("password123")
+                .build();
+
+        String confirmPassword = "wrongPassword";
+
+        // when & then
+        assertThrows(IllegalArgumentException.class, () -> user.confirmPassword(confirmPassword));
+    }
+}
