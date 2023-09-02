478b54f6f3422938629271330273be29cf2d21e2
joyfulviper <kimsk3113@naver.com>
2023-09-02 15:55:12 +0900

    [MDH-29] refactor : 정규식 클래스로 분리

--- src/main/java/com/mdh/devtable/user/presentation/SignUpRequest.java
+++ src/main/java/com/mdh/devtable/user/presentation/SignUpRequest.java
@@ -1,5 +1,6 @@
 package com.mdh.devtable.user.presentation;
 
+import com.mdh.devtable.global.util.RegularExpression;
 import com.mdh.devtable.user.domain.Role;
 import com.mdh.devtable.user.domain.User;
 import jakarta.validation.constraints.Email;
@@ -7,11 +8,15 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 
 public record SignUpRequest(
+
+        @Pattern(regexp = RegularExpression.EMAIL, message = "이메일 형식에 맞게 입력해 주세요.")
         @Email(message = "이메일 형식에 맞게 입력해 주세요.")
         String email,
-        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=\\S+$).{8,}$",
+
+        @Pattern(regexp = RegularExpression.PASSWORD,
                 message = "비밀번호는 최소 8글자의 최소 1개 이상의 문자, 1개 이상의 숫자가 포함 되어야 합니다.")
         String password,
+
         @NotEmpty(message = "비밀번호 확인을 입력해 주세요.")
         String passwordCheck
 ) {
