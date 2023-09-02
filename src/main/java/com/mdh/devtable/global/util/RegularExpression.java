8403e044beaa312394e1112bbe22e4997c28d3fc
joyfulviper <kimsk3113@naver.com>
2023-09-02 15:55:12 +0900

    [MDH-29] refactor : 정규식 클래스로 분리

---
+++ src/main/java/com/mdh/devtable/global/util/RegularExpression.java
@@ -0,0 +1,8 @@
+package com.mdh.devtable.global.util;
+
+public class RegularExpression {
+
+    public static final String EMAIL = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+
+    public static final String PASSWORD = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=\\S+$).{8,}$";
+}
