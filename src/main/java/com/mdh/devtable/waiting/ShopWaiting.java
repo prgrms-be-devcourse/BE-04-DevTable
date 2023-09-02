c21d0c3e635f907935ccd3bdbe25289a37eba756
Bombo <bombo96@chungwoon.ac.kr>
2023-09-02 23:27:37 +0900

    [MDH-9] feat : 매장-웨이팅 엔티티 구현

---
+++ src/main/java/com/mdh/devtable/waiting/ShopWaiting.java
@@ -0,0 +1,56 @@
+package com.mdh.devtable.waiting;
+
+import com.mdh.devtable.global.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "shop_waitings")
+@Entity
+public class ShopWaiting extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "shop_id", nullable = false)
+    private Long shopId;
+
+    @Column(name = "status", length = 31, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ShopWaitingStatus shopWaitingStatus;
+
+    @Column(name = "maximum", nullable = false)
+    private int maximumWaiting;
+
+    @Builder
+    public ShopWaiting(Long shopId, int maximumWaiting) {
+        validMaximumWaiting(maximumWaiting);
+        this.shopId = shopId;
+        this.maximumWaiting = maximumWaiting;
+        this.shopWaitingStatus = ShopWaitingStatus.CLOSE;
+    }
+
+    public void changeShopWaitingStatus(ShopWaitingStatus shopWaitingStatus) {
+        if (this.shopWaitingStatus == shopWaitingStatus) {
+            throw new IllegalStateException("매장의 웨이팅 상태를 동일한 상태로 변경 할 수 없습니다.");
+        }
+
+        this.shopWaitingStatus = shopWaitingStatus;
+    }
+
+    public void updateShopWaiting(int maximumWaiting) {
+        validMaximumWaiting(maximumWaiting);
+        this.maximumWaiting = maximumWaiting;
+    }
+
+    private void validMaximumWaiting(int maximumWaiting) {
+        if (maximumWaiting < 1) {
+            throw new IllegalArgumentException("웨이팅의 최대 인원수는 1 미만 일 수 없습니다.");
+        }
+    }
+}
