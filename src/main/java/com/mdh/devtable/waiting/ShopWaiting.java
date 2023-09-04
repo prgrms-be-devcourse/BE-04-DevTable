3862d65cf4df6a4edf2b59f8eae527741785dc4d
heenahan <mby1997s@gmail.com>
2023-09-04 17:56:25 +0900

    [MDH-44] feat : 매장 웨이팅 유아 허용, 최대 최소 인원수 필드 추가

--- src/main/java/com/mdh/devtable/waiting/ShopWaiting.java
+++ src/main/java/com/mdh/devtable/waiting/ShopWaiting.java
@@ -1,7 +1,12 @@
 package com.mdh.devtable.waiting;
 
 import com.mdh.devtable.global.BaseTimeEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,15 +26,41 @@ public class ShopWaiting extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     private ShopWaitingStatus shopWaitingStatus;
 
-    @Column(name = "maximum", nullable = false)
+    @Column(name = "maximum", nullable = false) // 최대 웨이팅 팀 수
     private int maximumWaiting;
 
+    @Column(name = "child_enabled", nullable = false)
+    private boolean childEnabled;
+    // 최소 웨이팅 인원, 최대 웨이팅 인원
+    @Column(name = "minimum_people", nullable = false)
+    private int minimumWaitingPeople;
+
+    @Column(name = "maximum_people", nullable = false)
+    private int maximumWaitingPeople;
+
     @Builder
-    public ShopWaiting(Long shopId, int maximumWaiting) {
+    public ShopWaiting(Long shopId,
+                       int maximumWaiting,
+                       int minimumWaitingPeople,
+                       int maximumWaitingPeople) {
         validMaximumWaiting(maximumWaiting);
         this.shopId = shopId;
         this.maximumWaiting = maximumWaiting;
         this.shopWaitingStatus = ShopWaitingStatus.CLOSE;
+        this.childEnabled = false;
+        this.minimumWaitingPeople = minimumWaitingPeople;
+        this.maximumWaitingPeople = maximumWaitingPeople;
+    }
+
+    private void validMaximumWaiting(int maximumWaiting) {
+        if (maximumWaiting < 1) {
+            throw new IllegalArgumentException("웨이팅의 최대 인원수는 1 미만 일 수 없습니다.");
+        }
+    }
+
+    public void updateShopWaiting(int maximumWaiting) {
+        validMaximumWaiting(maximumWaiting);
+        this.maximumWaiting = maximumWaiting;
     }
 
     public void changeShopWaitingStatus(ShopWaitingStatus shopWaitingStatus) {
@@ -40,18 +71,23 @@ public class ShopWaiting extends BaseTimeEntity {
         this.shopWaitingStatus = shopWaitingStatus;
     }
 
-    public void updateShopWaiting(int maximumWaiting) {
-        validMaximumWaiting(maximumWaiting);
-        this.maximumWaiting = maximumWaiting;
-    }
-
     public boolean isOpenWaitingStatus() {
         return this.shopWaitingStatus == ShopWaitingStatus.OPEN;
     }
 
-    private void validMaximumWaiting(int maximumWaiting) {
-        if (maximumWaiting < 1) {
-            throw new IllegalArgumentException("웨이팅의 최대 인원수는 1 미만 일 수 없습니다.");
+    public void updateChildEnabled(boolean childEnabled) {
+        this.childEnabled = childEnabled;
+    }
+
+    public void validOverMinimumPeople(int people) {
+        if (people < minimumWaitingPeople) {
+            throw new IllegalArgumentException("웨이팅 인원은 " + minimumWaitingPeople + "명 이상이어야 합니다.");
+        }
+    }
+
+    public void validUnderMaximumPeople(int people) {
+        if (people > maximumWaitingPeople) {
+            throw new IllegalArgumentException("웨이팅 인원은 " + maximumWaitingPeople + "명 이하여야 합니다.");
         }
     }
 }
