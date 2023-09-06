3207173a04f3aa7bb6e531e733f6c839dad87286
heenahan <mby1997s@gmail.com>
2023-09-06 15:22:54 +0900

    [MDH-15] refactor : waiting line 테스트 케이스 추가

--- src/test/java/com/mdh/devtable/waiting/WaitingLineTest.java
+++ src/test/java/com/mdh/devtable/waiting/WaitingLineTest.java
@@ -1,5 +1,6 @@
 package com.mdh.devtable.waiting;
 
+import com.mdh.devtable.waiting.infra.persistence.PlainWaitingLine;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,16 +18,19 @@ class WaitingLineTest {
         var waitingId1 = 1L;
         var waitingId1Time = LocalDateTime.now();
         var waitingId2 = 2L;
-        var waitingId2Time = LocalDateTime.now().plusMinutes(5);
+        var waitingId2Time = waitingId1Time.plusMinutes(1);
+        var waitingId3 = 3L;
+        var waitingId3Time = waitingId2Time.plusMinutes(1);
 
-        var waitingLine = new WaitingLine();
+        var waitingLine = new PlainWaitingLine();
         waitingLine.save(shopId, waitingId1, waitingId1Time);
         waitingLine.save(shopId, waitingId2, waitingId2Time);
+        waitingLine.save(shopId, waitingId3, waitingId3Time);
 
         //when
-        var rank = waitingLine.findRank(shopId, waitingId2, waitingId2Time);
+        var rank = waitingLine.findRank(shopId, waitingId3, waitingId3Time);
 
         //then
-        assertThat(rank).isEqualTo(2);
+        assertThat(rank).isEqualTo(3);
     }
 }
