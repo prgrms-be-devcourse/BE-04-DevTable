7110d45bd1b2e53ce26ddfb082a2436cca87d97d
joyfulviper <kimsk3113@naver.com>
2023-09-08 00:31:27 +0900

    [MDH-24] feat : 점주 웨이팅 상태 변경 기능, 테스트 추가

--- src/main/java/com/mdh/devtable/ownerwaitng/infra/persistence/OwnerWaitingRepository.java
+++ src/main/java/com/mdh/devtable/ownerwaitng/infra/persistence/OwnerWaitingRepository.java
@@ -2,10 +2,14 @@ package com.mdh.devtable.ownerwaitng.infra.persistence;
 
 
 import com.mdh.devtable.waiting.domain.ShopWaiting;
+import com.mdh.devtable.waiting.domain.Waiting;
 
 import java.util.Optional;
 
 public interface OwnerWaitingRepository {
 
-    Optional<ShopWaiting> findByShopId(Long shopId);
+    Optional<ShopWaiting> findShopWaitingByShopId(Long shopId);
+
+    Optional<Waiting> findWaitingByWaitingId(Long waitingId);
+
 }
