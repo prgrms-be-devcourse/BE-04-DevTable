6a7c61edad0bee1a06343d9228c01ab9b4e80f63
joyfulviper <kimsk3113@naver.com>
2023-09-08 02:46:42 +0900

    [MDH-48] feat : 웨이팅 상태 변경 엔드포인트, 테스트 코드 작성

--- src/main/java/com/mdh/devtable/ownerwaitng/presentaion/controller/OwnerWaitingController.java
+++ src/main/java/com/mdh/devtable/ownerwaitng/presentaion/controller/OwnerWaitingController.java
@@ -3,6 +3,7 @@ package com.mdh.devtable.ownerwaitng.presentaion.controller;
 import com.mdh.devtable.global.ApiResponse;
 import com.mdh.devtable.ownerwaitng.application.OwnerWaitingService;
 import com.mdh.devtable.ownerwaitng.presentaion.dto.OwnerShopWaitingStatusChangeRequest;
+import com.mdh.devtable.ownerwaitng.presentaion.dto.OwnerWaitingStatusChangeRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,12 @@ public class OwnerWaitingController {
     @PatchMapping("/shops/{shopId}")
     public ResponseEntity<ApiResponse<Void>> changShopWaitingStatus(@RequestBody OwnerShopWaitingStatusChangeRequest request, @PathVariable("shopId") Long shopId) {
         ownerWaitingService.changeShopWaitingStatus(shopId, request);
+        return new ResponseEntity<>(ApiResponse.ok(null), HttpStatus.OK);
+    }
+
+    @PatchMapping("/waitings/{waitingId}")
+    public ResponseEntity<ApiResponse<Void>> changeWaitingStatus(@RequestBody OwnerWaitingStatusChangeRequest request, @PathVariable("waitingId") Long waitingId) {
+        ownerWaitingService.changeWaitingStatus(waitingId, request);
         return new ResponseEntity<>(ApiResponse.ok(null), HttpStatus.OK);
     }
 }
