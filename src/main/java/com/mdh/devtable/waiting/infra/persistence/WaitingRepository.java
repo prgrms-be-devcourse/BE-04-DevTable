f549be6096f54ad6aa3e7b56642891d60cdc7346
Bombo <bombo96@chungwoon.ac.kr>
2023-09-07 19:08:37 +0900

    [MDH-52] feat : 웨이팅의 발급번호를 부여하는 기능 추가

--- src/main/java/com/mdh/devtable/waiting/infra/persistence/WaitingRepository.java
+++ src/main/java/com/mdh/devtable/waiting/infra/persistence/WaitingRepository.java
@@ -2,6 +2,13 @@ package com.mdh.devtable.waiting.infra.persistence;
 
 import com.mdh.devtable.waiting.domain.Waiting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface WaitingRepository extends JpaRepository<Waiting, Long> {
+
+    @Query("select w from Waiting w where w.waitingStatus = 'PROGRESS' and w.userId = :userId")
+    Optional<Waiting> findByProgressWaiting(@Param("userId") Long userId);
 }
