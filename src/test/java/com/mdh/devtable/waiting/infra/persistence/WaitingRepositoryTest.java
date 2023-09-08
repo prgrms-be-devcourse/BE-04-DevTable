b5d64a261144a90701a09ab5008b7ecf21cc3d1f
heenahan <mby1997s@gmail.com>
2023-09-08 17:34:33 +0900

    [MDH-16] test : 웨이팅 상세 조회 repository 테스트

--- src/test/java/com/mdh/devtable/waiting/infra/persistence/WaitingRepositoryTest.java
+++ src/test/java/com/mdh/devtable/waiting/infra/persistence/WaitingRepositoryTest.java
@@ -8,6 +8,7 @@ import com.mdh.devtable.user.infra.persistence.UserRepository;
 import com.mdh.devtable.waiting.domain.ShopWaitingStatus;
 import com.mdh.devtable.waiting.domain.WaitingStatus;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -79,5 +80,43 @@ class WaitingRepositoryTest {
         assertThat(findUserWaitings).hasSize(1);
         assertThat(findUserWaitings).extracting("shopId", "waitingId")
                 .contains(tuple(shop.getId(), waiting.getId()));
+    }
+
+    @Test
+    @DisplayName("웨이팅 아이디로 웨이팅 상세 정보를 가져온다.")
+    void findWaitingDetails() {
+        //given
+        var guest = DataInitializerFactory.guest();
+        var savedUser = userRepository.save(guest);
+
+        var region = DataInitializerFactory.region();
+        regionRepository.save(region);
+
+        var shopDetails = DataInitializerFactory.shopDetails();
+        var shopAddress = DataInitializerFactory.shopAddress();
+        var shop = DataInitializerFactory.shop(guest.getId(), shopDetails, region, shopAddress);
+        var savedShop = shopRepository.save(shop);
+
+        var shopWaiting = DataInitializerFactory.shopWaiting(savedUser.getId(), 20, 7, 2);
+        shopWaiting.changeShopWaitingStatus(ShopWaitingStatus.OPEN);
+        var savedShopWaiting = shopWaitingRepository.save(shopWaiting);
+
+        var waitingPeople = DataInitializerFactory.waitingPeople(2, 0);
+        var waiting = DataInitializerFactory.waiting(savedUser.getId(), shopWaiting, waitingPeople);
+        var savedWaiting = waitingRepository.save(waiting);
+
+        //when
+        var waitingDetails = waitingRepository.findByWaitingDetails(savedWaiting.getId()).orElse(null);
+
+        //then
+        assertThat(waitingDetails.shopName()).isEqualTo(shop.getName());
+        assertThat(waitingDetails.region()).isEqualTo(region.getDistrict());
+        assertThat(waitingDetails.shopType()).isEqualTo(shop.getShopType());
+        assertThat(waitingDetails.shopDetails()).isEqualTo(shopDetails);
+        assertThat(waitingDetails.waitingNumber()).isEqualTo(waiting.getWaitingNumber());
+        assertThat(waitingDetails.waitingStatus()).isEqualTo(waiting.getWaitingStatus());
+        assertThat(waitingDetails.waitingPeople()).isEqualTo(waitingPeople);
+        assertThat(waitingDetails.createdDate()).isEqualTo(waiting.getCreatedDate());
+        assertThat(waitingDetails.modifiedDate()).isEqualTo(waiting.getModifiedDate());
     }
 }
