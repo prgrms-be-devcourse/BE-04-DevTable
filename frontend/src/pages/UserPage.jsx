701a15b22b47ed4669a8247397928b5fd2acdcb7
Bombo <bombo96@chungwoon.ac.kr>
2023-09-10 01:23:49 +0900

    feat : 유저 페이지를 호출하는 기능 페이지 추가

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -1,5 +1,49 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+const waitingId = '1';
+
+const uri = `http://localhost:8080/api/v1/customer/${waitingId}`;
+
+const fetchWaitingData = async () => {
+  try {
+    const response = await fetch(uri);
+    if (!response.ok) {
+      throw new Error('데이터를 가져오는 데 실패했습니다.');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error('데이터를 가져오는 중 오류 발생:', error);
+  }
+};
 
 export default function UserPage() {
-  return <div>UserPage</div>;
+  const {
+    data: waitingData,
+    isLoading,
+    isError,
+    error,
+  } = useQuery('waitingData', fetchWaitingData);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>UserPage - 웨이팅 확인</h1>
+      <ul>
+        {waitingData.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.waitingTime}분
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
