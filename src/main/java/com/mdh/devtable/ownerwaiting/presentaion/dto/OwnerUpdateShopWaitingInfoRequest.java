e27d01c5d73973bce7356d955470eb7f51b2c23a
joyfulviper <kimsk3113@naver.com>
2023-09-10 20:13:07 +0900

    [MDH-145] feat & test : 매장의 웨이팅 정보를 변경하는 엔드포인트 구현 및 rest docs 작성

--- src/main/java/com/mdh/devtable/ownerwaiting/presentaion/dto/OwnerUpdateShopWaitingInfoRequest.java
+++ src/main/java/com/mdh/devtable/ownerwaiting/presentaion/dto/OwnerUpdateShopWaitingInfoRequest.java
@@ -1,9 +1,20 @@
 package com.mdh.devtable.ownerwaiting.presentaion.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 public record OwnerUpdateShopWaitingInfoRequest(
+
+        @NotNull
         Boolean childEnabled,
+
+        @Min(1) @NotNull
         Integer maximumPeople,
+
+        @Min(1) @NotNull
         Integer minimumPeople,
+
+        @Min(1) @NotNull
         Integer maximumWaitingTeam
 ) {
 }
