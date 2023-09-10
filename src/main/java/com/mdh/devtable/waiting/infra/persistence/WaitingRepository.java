a137cd61ae8659483c0e186f7ceed2c6168ee53c
joyfulviper <kimsk3113@naver.com>
2023-09-10 16:32:27 +0900

    [MDH-144] feat & test : 매장의 웨이팅 정보를 변경하는 기능 추가

--- src/main/java/com/mdh/devtable/waiting/infra/persistence/WaitingRepository.java
+++ src/main/java/com/mdh/devtable/waiting/infra/persistence/WaitingRepository.java
@@ -18,12 +18,11 @@ public interface WaitingRepository extends JpaRepository<Waiting, Long> {
     Optional<Waiting> findByProgressWaiting(@Param("userId") Long userId);
 
     @Query("""
-            <<<<<<< HEAD
-                         select new com.mdh.devtable.waiting.infra.persistence.dto.UserWaitingQueryDto(s.id, w.id, s.name, s.shopType, s.region.city, s.region.district, w.waitingNumber, w.waitingPeople.adultCount, w.waitingPeople.childCount)
-                         from Waiting w
-                         join Shop s on w.shopWaiting.shopId = s.id
-                         where w.userId = :userId and w.waitingStatus = :waitingStatus
-                        """)
+             select new com.mdh.devtable.waiting.infra.persistence.dto.UserWaitingQueryDto(s.id, w.id, s.name, s.shopType, s.region.city, s.region.district, w.waitingNumber, w.waitingPeople.adultCount, w.waitingPeople.childCount)
+             from Waiting w
+             join Shop s on w.shopWaiting.shopId = s.id
+             where w.userId = :userId and w.waitingStatus = :waitingStatus
+            """)
     List<UserWaitingQueryDto> findAllByUserIdAndWaitingStatus(@Param("userId") Long userId, @Param("waitingStatus") WaitingStatus waitingStatus);
 
     @Query("""
