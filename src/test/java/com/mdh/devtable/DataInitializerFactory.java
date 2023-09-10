4d8385dea0a3bd4a49b12a98e4a15b6e32048578
joyfulviper <kimsk3113@naver.com>
2023-09-11 00:47:53 +0900

    [MDH-59] refactor : User 전화번호 필드 추가

--- src/test/java/com/mdh/devtable/DataInitializerFactory.java
+++ src/test/java/com/mdh/devtable/DataInitializerFactory.java
@@ -14,6 +14,7 @@ public final class DataInitializerFactory {
                 .email("owner@example.com")
                 .role(Role.OWNER)
                 .password("password123")
+                .phoneNumber("01056781234")
                 .build();
     }
 
@@ -22,6 +23,7 @@ public final class DataInitializerFactory {
                 .email("guest@example.com")
                 .role(Role.GUEST)
                 .password("password123")
+                .phoneNumber("01012345678")
                 .build();
     }
 
