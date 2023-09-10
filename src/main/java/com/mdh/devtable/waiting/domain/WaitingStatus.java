a48602e028c9c1c93908d4bf2612728412085b40
heenahan <mby1997s@gmail.com>
2023-09-11 01:00:16 +0900

    [MDH-16] refactor : repository는 DataJpaTest로 변경, 상태를 가진 객체에게 책임 넘김

--- src/main/java/com/mdh/devtable/waiting/domain/WaitingStatus.java
+++ src/main/java/com/mdh/devtable/waiting/domain/WaitingStatus.java
@@ -2,5 +2,12 @@ package com.mdh.devtable.waiting.domain;
 
 public enum WaitingStatus {
 
-    PROGRESS, CANCEL, NO_SHOW, VISITED
+    PROGRESS,
+    CANCEL,
+    NO_SHOW,
+    VISITED;
+
+    public boolean isProgress() {
+        return this == WaitingStatus.PROGRESS;
+    }
 }
