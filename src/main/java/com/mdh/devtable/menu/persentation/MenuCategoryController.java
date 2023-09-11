5ea4e79169be6a178abbb11604df19c17f9ecd74
joyfulviper <kimsk3113@naver.com>
2023-09-12 02:27:07 +0900

    [MDH-153] feat : 점주가 메뉴 카테고리를 등록하는 rest docs, validate, index.adoc 추가

--- src/main/java/com/mdh/devtable/menu/persentation/MenuCategoryController.java
+++ src/main/java/com/mdh/devtable/menu/persentation/MenuCategoryController.java
@@ -3,6 +3,7 @@ package com.mdh.devtable.menu.persentation;
 import com.mdh.devtable.global.ApiResponse;
 import com.mdh.devtable.menu.application.MenuCategoryService;
 import com.mdh.devtable.menu.persentation.dto.MenuCategoryCreateRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ public class MenuCategoryController {
     private final MenuCategoryService menuCategoryService;
 
     @PostMapping("/api/owner/v1/shops/{shopId}/categories")
-    public ResponseEntity<ApiResponse<Void>> createMenuCategory(@PathVariable("shopId") Long shopId, @RequestBody MenuCategoryCreateRequest request) {
+    public ResponseEntity<ApiResponse<Void>> createMenuCategory(@PathVariable("shopId") Long shopId, @Valid @RequestBody MenuCategoryCreateRequest request) {
         menuCategoryService.createMenuCategory(shopId, request);
         return new ResponseEntity<>(ApiResponse.created(null), HttpStatus.CREATED);
     }
