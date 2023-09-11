83d79efb2349d4efbcecef2378f6f7d11eafe402
joyfulviper <kimsk3113@naver.com>
2023-09-12 02:10:55 +0900

    [MDH-153] feat : 점주가 메뉴 카테고리를 등록하는 엔드포인트 구현 및 rest docs 추가

---
+++ src/main/java/com/mdh/devtable/menu/persentation/MenuCategoryController.java
@@ -0,0 +1,25 @@
+package com.mdh.devtable.menu.persentation;
+
+import com.mdh.devtable.global.ApiResponse;
+import com.mdh.devtable.menu.application.MenuCategoryService;
+import com.mdh.devtable.menu.persentation.dto.MenuCategoryCreateRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class MenuCategoryController {
+
+    private final MenuCategoryService menuCategoryService;
+
+    @PostMapping("/api/owner/v1/shops/{shopId}/categories")
+    public ResponseEntity<ApiResponse<Void>> createMenuCategory(@PathVariable("shopId") Long shopId, @RequestBody MenuCategoryCreateRequest request) {
+        menuCategoryService.createMenuCategory(shopId, request);
+        return new ResponseEntity<>(ApiResponse.created(null), HttpStatus.CREATED);
+    }
+}
