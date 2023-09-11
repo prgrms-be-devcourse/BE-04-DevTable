5ae4b8f133ba90465763c233f168dacdf0f1ab5d
Bombo <bombo96@chungwoon.ac.kr>
2023-09-12 00:31:57 +0900

    [MDH-112] feat & test : 웨이팅 미루기 구현 및 테스트

--- src/main/java/com/mdh/devtable/waiting/infra/persistence/PlainWaitingLine.java
+++ src/main/java/com/mdh/devtable/waiting/infra/persistence/PlainWaitingLine.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.Map;
-import java.util.SortedSet;
 import java.util.TreeSet;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -14,22 +13,21 @@ import java.util.concurrent.ConcurrentHashMap;
 public class PlainWaitingLine implements WaitingLine {
 
     // 매장의 아이디, 매장의 웨이팅 라인
-    private final Map<Long, SortedSet<WaitingInfo>> waitingLine = new ConcurrentHashMap<>();
+    private final Map<Long, TreeSet<WaitingInfo>> waitingLine = new ConcurrentHashMap<>();
 
     @Override
-    public void save(Long shopId, Long waitingId, LocalDateTime createdDate) {
-        // 해당 매장에 대한 라인이 없다면
+    public void save(Long shopId, Long waitingId, LocalDateTime issuedTime) {
         waitingLine.putIfAbsent(shopId,
                 new TreeSet<>(Comparator.comparing(WaitingInfo::waitingStartTime)));
 
-        var waitingInfo = new WaitingInfo(waitingId, createdDate);
+        var waitingInfo = new WaitingInfo(waitingId, issuedTime);
         var waitingInfos = waitingLine.get(shopId);
         waitingInfos.add(waitingInfo);
     }
 
     @Override
-    public int findRank(Long shopId, Long waitingId, LocalDateTime createdDate) {
-        var waitingInfo = new WaitingInfo(waitingId, createdDate);
+    public int findRank(Long shopId, Long waitingId, LocalDateTime issuedTime) {
+        var waitingInfo = new WaitingInfo(waitingId, issuedTime);
         var waitingInfos = waitingLine.get(shopId);
 
         return waitingInfos.headSet(waitingInfo).size() + 1;
@@ -41,14 +39,40 @@ public class PlainWaitingLine implements WaitingLine {
         return waitingInfos.size();
     }
 
-    public void cancel(Long shopId, Long waitingId, LocalDateTime createdDate) {
-        if (!waitingLine.containsKey(shopId)) {
-            throw new IllegalStateException("현재 매장에 등록 된 웨이팅이 아닙니다. shopId : " + shopId + "waitingId : " + waitingId);
-        }
+    @Override
+    public void cancel(Long shopId, Long waitingId, LocalDateTime issuedTime) {
+        var waitingInfo = new WaitingInfo(waitingId, issuedTime);
+        validExistWaiting(shopId, waitingInfo);
 
-        var waitingInfo = new WaitingInfo(waitingId, createdDate);
         var waitingInfos = waitingLine.get(shopId);
         waitingInfos.remove(waitingInfo);
 
+    }
+
+    private void validExistWaiting(Long shopId, WaitingInfo waitingInfo) {
+        if (!waitingLine.containsKey(shopId)) {
+            throw new IllegalStateException("현재 매장에 등록 된 웨이팅이 아닙니다. shopId : " + shopId +
+                    "waitingId : " + waitingInfo.waitingId());
+        }
+
+        if (!waitingLine.get(shopId).contains(waitingInfo)) {
+            throw new IllegalStateException("현재 매장에 등록 된 웨이팅이 아닙니다. shopId : " + shopId +
+                    "waitingId : " + waitingInfo.waitingId());
+        }
+    }
+
+    @Override
+    public void postpone(Long shopId, Long waitingId, LocalDateTime preIssuedTime, LocalDateTime issuedTime) {
+        var waitingInfo = new WaitingInfo(waitingId, preIssuedTime);
+        validExistWaiting(shopId, waitingInfo);
+
+        var waitingInfos = waitingLine.get(shopId);
+        waitingInfos.remove(waitingInfo);
+        waitingInfos.add(new WaitingInfo(waitingId, issuedTime));
+    }
+
+    @Override
+    public boolean isPostpone(Long shopId, Long waitingId, LocalDateTime issuedTime) {
+        return findRank(shopId, waitingId, issuedTime) != findTotalWaiting(shopId);
     }
 }
--- src/test/java/com/mdh/devtable/waiting/WaitingLineTest.java
+++ src/test/java/com/mdh/devtable/waiting/WaitingLineTest.java
@@ -51,14 +51,14 @@ class WaitingLineTest {
         waitingLine.save(shopId, waitingId1, waitingId1Time);
         waitingLine.save(shopId, waitingId2, waitingId2Time);
         waitingLine.save(shopId, waitingId3, waitingId3Time);
-    
+
         //when
         var totalWaiting = waitingLine.findTotalWaiting(shopId);
-      
+
         //then
         assertThat(totalWaiting).isEqualTo(3);
     }
-  
+
     @DisplayName("매장에서 웨이팅이 취소가 되면 순위가 변경된다.")
     void cancelWaitingTest() {
         //given
@@ -96,5 +96,54 @@ class WaitingLineTest {
         assertThatThrownBy(() -> waitingLine.cancel(shopId, waitingId, waitingTime))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage("현재 매장에 등록 된 웨이팅이 아닙니다. shopId : " + shopId + "waitingId : " + waitingId);
+    }
+
+    @Test
+    @DisplayName("매장에 등록된 웨이팅을 맨 뒤로 미룰 수 있다.")
+    void postponeWaitingTest() {
+        //given
+        var shopId = 1L;
+        var waitingId1 = 1L;
+        var waitingId1Time = LocalDateTime.now();
+        var waitingId2 = 2L;
+        var waitingId2Time = waitingId1Time.plusMinutes(1);
+        var waitingId3 = 3L;
+        var waitingId3Time = waitingId2Time.plusMinutes(2);
+
+        var waitingLine = new PlainWaitingLine();
+        waitingLine.save(shopId, waitingId1, waitingId1Time);
+        waitingLine.save(shopId, waitingId2, waitingId2Time);
+        waitingLine.save(shopId, waitingId3, waitingId3Time);
+
+        //when
+        waitingLine.postpone(shopId, waitingId1, waitingId1Time, waitingId3Time.plusMinutes(1));
+        var findWaiting = waitingLine.findRank(shopId, waitingId1, waitingId3Time.plusMinutes(1));
+
+        //then
+        assertThat(findWaiting).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("맨 뒤에 있는 웨이팅은 미룰 수 없다.")
+    void postponeWaitingFailTest() {
+        //given
+        var shopId = 1L;
+        var waitingId1 = 1L;
+        var waitingId1Time = LocalDateTime.now();
+        var waitingId2 = 2L;
+        var waitingId2Time = waitingId1Time.plusMinutes(1);
+        var waitingId3 = 3L;
+        var waitingId3Time = waitingId2Time.plusMinutes(2);
+
+        var waitingLine = new PlainWaitingLine();
+        waitingLine.save(shopId, waitingId1, waitingId1Time);
+        waitingLine.save(shopId, waitingId2, waitingId2Time);
+        waitingLine.save(shopId, waitingId3, waitingId3Time);
+
+        //when
+        var isPostpone = waitingLine.isPostpone(shopId, waitingId3, waitingId3Time);
+
+        //then
+        assertThat(isPostpone).isEqualTo(false);
     }
 }
