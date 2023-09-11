8a701bc52f286e934f30aeb1def77aab4433878e
joyfulviper <kimsk3113@naver.com>
2023-09-11 23:48:22 +0900

    [MDH-152] refactor : dto valdation 설명, 이름 최솟값 설정

--- src/main/java/com/mdh/devtable/menu/persentation/dto/MenuCategoryCreateRequest.java
+++ src/main/java/com/mdh/devtable/menu/persentation/dto/MenuCategoryCreateRequest.java
@@ -5,12 +5,15 @@ import com.mdh.devtable.menu.domain.MenuCategory;
 import com.mdh.devtable.menu.domain.MenuType;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record MenuCategoryCreateRequest(
 
+        @Size(min = 1, max = 31, message = "메뉴 카테고리 이름은 1~31자 이내로 입력해 주세요")
         @NotNull(message = "메뉴 카테고리 이름을 입력해 주세요")
         String name,
 
+        @Size(max = 31, message = "메뉴 카테고리 설명은 31자 이내로 해주세요")
         String description,
 
         @Min(value = 1, message = "1 이상의 값을 입력해 주세요")
