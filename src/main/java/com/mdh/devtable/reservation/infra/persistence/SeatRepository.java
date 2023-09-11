0829094a4e39b13ec6ab28b61e5beba4e95684ec
heenahan <mby1997s@gmail.com>
2023-09-11 15:44:16 +0900

    [MDH-62] fix : jpa repository 파라미터 변경

--- src/main/java/com/mdh/devtable/reservation/infra/persistence/SeatRepository.java
+++ src/main/java/com/mdh/devtable/reservation/infra/persistence/SeatRepository.java
@@ -1,6 +1,7 @@
 package com.mdh.devtable.reservation.infra.persistence;
 
+import com.mdh.devtable.reservation.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SeatRepository extends JpaRepository<SeatRepository, Long> {
+public interface SeatRepository extends JpaRepository<Seat, Long> {
 }
