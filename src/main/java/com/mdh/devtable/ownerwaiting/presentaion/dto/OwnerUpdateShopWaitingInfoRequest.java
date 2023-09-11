dd3244c8a5722315101bcf8e82175b1c19bf7dc1
joyfulviper <kimsk3113@naver.com>
2023-09-11 17:11:44 +0900

    [MDH-145] refactor : dto 검증 메시지 추가

--- src/main/java/com/mdh/devtable/ownerwaiting/presentaion/dto/OwnerUpdateShopWaitingInfoRequest.java
+++ src/main/java/com/mdh/devtable/ownerwaiting/presentaion/dto/OwnerUpdateShopWaitingInfoRequest.java
@@ -11,17 +11,17 @@ public record OwnerUpdateShopWaitingInfoRequest(
 
         @Min(value = 1, message = "1이상의 값만 입력 해주세요")
         @Max(value = Integer.MAX_VALUE, message = "너무 큰 입력 값입니다.")
-        @NotNull
+        @NotNull(message = "최대 인원 수를 작성해 주세요.")
         Integer maximumPeople,
 
         @Min(value = 1, message = "1이상의 값만 입력 해주세요")
         @Max(value = Integer.MAX_VALUE, message = "너무 큰 입력 값입니다.")
-        @NotNull
+        @NotNull(message = "최소 인원 수를 작성해 주세요.")
         Integer minimumPeople,
 
         @Min(value = 1, message = "1이상의 값만 입력 해주세요")
         @Max(value = Integer.MAX_VALUE, message = "너무 큰 입력 값입니다.")
-        @NotNull
+        @NotNull(message = "예약을 받을 최대 팀을 작성해 주세요.")
         Integer maximumWaitingTeam
 ) {
 }
