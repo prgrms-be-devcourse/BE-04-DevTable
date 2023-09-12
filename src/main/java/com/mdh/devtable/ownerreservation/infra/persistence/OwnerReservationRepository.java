aac06fd1c22b96901adc32814f7a472e7c821c91
joyfulviper <kimsk3113@naver.com>
2023-09-13 04:06:34 +0900

    [MDH-159] feat & test : 점주가 매장의 좌석을 등록하는 기능 구현 및 테스트

--- src/main/java/com/mdh/devtable/ownerreservation/infra/persistence/OwnerReservationRepository.java
+++ src/main/java/com/mdh/devtable/ownerreservation/infra/persistence/OwnerReservationRepository.java
@@ -1,8 +1,15 @@
 package com.mdh.devtable.ownerreservation.infra.persistence;
 
+import com.mdh.devtable.reservation.domain.Seat;
 import com.mdh.devtable.reservation.domain.ShopReservation;
+
+import java.util.Optional;
 
 public interface OwnerReservationRepository {
 
     Long saveShopReservation(ShopReservation shopReservation);
+
+    Optional<ShopReservation> findShopReservationByShopId(Long shopId);
+
+    Long saveSeat(Seat seat);
 }
--- src/main/java/com/mdh/devtable/ownerreservation/infra/persistence/OwnerReservationRepositoryImpl.java
+++ src/main/java/com/mdh/devtable/ownerreservation/infra/persistence/OwnerReservationRepositoryImpl.java
@@ -1,18 +1,33 @@
 package com.mdh.devtable.ownerreservation.infra.persistence;
 
+import com.mdh.devtable.reservation.domain.Seat;
 import com.mdh.devtable.reservation.domain.ShopReservation;
+import com.mdh.devtable.reservation.infra.persistence.SeatRepository;
 import com.mdh.devtable.reservation.infra.persistence.ShopReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Repository
 public class OwnerReservationRepositoryImpl implements OwnerReservationRepository {
 
     private final ShopReservationRepository shopReservationRepository;
+    private final SeatRepository seatRepository;
 
     @Override
     public Long saveShopReservation(ShopReservation shopReservation) {
         return shopReservationRepository.save(shopReservation).getShopId();
+    }
+
+    @Override
+    public Optional<ShopReservation> findShopReservationByShopId(Long shopId) {
+        return shopReservationRepository.findById(shopId);
+    }
+
+    @Override
+    public Long saveSeat(Seat seat) {
+        return seatRepository.save(seat).getId();
     }
 }
