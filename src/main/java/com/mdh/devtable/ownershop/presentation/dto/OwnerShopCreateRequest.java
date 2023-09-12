e06319377ea520329cc7b359be1cc9101ac8ae42
joyfulviper <kimsk3113@naver.com>
2023-09-13 03:11:11 +0900

    [MDH-143] feat & test : 점주가 매장을 등록하는 엔드포인트 구현 및 rest docs 추가

--- src/main/java/com/mdh/devtable/ownershop/presentation/dto/OwnerShopCreateRequest.java
+++ src/main/java/com/mdh/devtable/ownershop/presentation/dto/OwnerShopCreateRequest.java
@@ -2,13 +2,28 @@ package com.mdh.devtable.ownershop.presentation.dto;
 
 import com.mdh.devtable.shop.Shop;
 import com.mdh.devtable.shop.ShopType;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record OwnerShopCreateRequest(
+
+        @NotBlank(message = "상점 이름은 필수입니다.")
         String name,
+
+        @NotBlank(message = "상점 설명은 필수입니다.")
         String description,
+
+        @NotNull(message = "상점 유형은 필수입니다.")
         ShopType shopType,
+
+        @Valid
         ShopDetailsRequest shopDetailsRequest,
+
+        @Valid
         ShopAddressRequest shopAddressRequest,
+
+        @Valid
         RegionRequest regionRequest
 
 ) {
