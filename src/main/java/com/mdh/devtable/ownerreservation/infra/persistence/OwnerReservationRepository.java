7bfe481c4b72d2fdcc4c6ea2acf7200d0f262954
joyfulviper <kimsk3113@naver.com>
2023-09-13 04:35:56 +0900

    [MDH-158] feat & test : 점주가 예약가능한 시간을 등록하는 기능 구현 및 테스트

--- src/main/java/com/mdh/devtable/ownerreservation/infra/persistence/OwnerReservationRepository.java
+++ src/main/java/com/mdh/devtable/ownerreservation/infra/persistence/OwnerReservationRepository.java
@@ -2,6 +2,7 @@ package com.mdh.devtable.ownerreservation.infra.persistence;
 
 import com.mdh.devtable.reservation.domain.Seat;
 import com.mdh.devtable.reservation.domain.ShopReservation;
+import com.mdh.devtable.reservation.domain.ShopReservationDateTime;
 
 import java.util.Optional;
 
@@ -12,4 +13,6 @@ public interface OwnerReservationRepository {
     Optional<ShopReservation> findShopReservationByShopId(Long shopId);
 
     Long saveSeat(Seat seat);
+
+    Long saveShopReservationDateTime(ShopReservationDateTime shopReservationDateTime);
 }
