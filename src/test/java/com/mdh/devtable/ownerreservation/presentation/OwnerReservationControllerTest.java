975bd5fb74e4c63d2a7c6d7b4d570fe6c70e108b
joyfulviper <kimsk3113@naver.com>
2023-09-13 05:21:05 +0900

    [MDH-162] feat & test : 점주가 고객을 받을 수 있는 예약 시간을 등록하는 엔드포인트 구현 및 rest docs 추가

--- src/test/java/com/mdh/devtable/ownerreservation/presentation/OwnerReservationControllerTest.java
+++ src/test/java/com/mdh/devtable/ownerreservation/presentation/OwnerReservationControllerTest.java
@@ -4,6 +4,7 @@ import com.mdh.devtable.RestDocsSupport;
 import com.mdh.devtable.ownerreservation.application.OwnerReservationService;
 import com.mdh.devtable.ownerreservation.presentation.dto.SeatCreateRequest;
 import com.mdh.devtable.ownerreservation.presentation.dto.ShopReservationCreateRequest;
+import com.mdh.devtable.ownerreservation.presentation.dto.ShopReservationDateTimeCreateRequest;
 import com.mdh.devtable.reservation.domain.SeatType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.HashMap;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -21,6 +26,7 @@ import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuild
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(OwnerReservationController.class)
@@ -163,6 +169,75 @@ class OwnerReservationControllerTest extends RestDocsSupport {
                                 fieldWithPath("data.type").type(JsonFieldType.STRING).description("타입"),
                                 fieldWithPath("data.validationError[].field").type(JsonFieldType.STRING).description("유효성 검사 실패 필드"),
                                 fieldWithPath("data.validationError[].message").type(JsonFieldType.STRING).description("유효성 검사 실패 메시지"),
+                                fieldWithPath("serverDateTime").type(JsonFieldType.STRING).description("서버 시간")
+                        )
+                ));
+    }
+
+    @DisplayName("점주는 매장의 예약 날짜와 시간을 생성할 수 있다.")
+    @Test
+    void createShopReservationDateTime() throws Exception {
+        var shopId = 1L;
+        var request = new ShopReservationDateTimeCreateRequest(LocalDate.of(2023, 9, 12), LocalTime.of(12, 30));
+        given(ownerReservationService.createShopReservationDateTime(any(Long.class), any(ShopReservationDateTimeCreateRequest.class))).willReturn(1L);
+
+        mockMvc.perform(post("/api/owner/v1/shops/{shopId}/reservation/date", shopId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", String.format("/api/owner/v1/shops/%d/reservation/date/%d", shopId, 1L)))
+                .andExpect(jsonPath("$.statusCode").value("201"))
+                .andExpect(jsonPath("$.data").doesNotExist())
+                .andExpect(jsonPath("$.serverDateTime").exists())
+                .andDo(print())
+                .andDo(document("owner-shop-reservation-date-create",
+                        pathParameters(
+                                parameterWithName("shopId").description("매장 id")
+                        ),
+                        requestFields(
+                                fieldWithPath("localDate").type(JsonFieldType.ARRAY).description("예약 날짜"),
+                                fieldWithPath("localTime").type(JsonFieldType.ARRAY).description("예약 시간")
+                        ),
+                        responseFields(
+                                fieldWithPath("statusCode").type(JsonFieldType.NUMBER).description("상태 코드"),
+                                fieldWithPath("data").type(JsonFieldType.NULL).description("응답 바디(비어 있음)"),
+                                fieldWithPath("serverDateTime").type(JsonFieldType.STRING).description("생성된 서버 시간")
+                        ),
+                        responseHeaders(
+                                headerWithName("Location").description("새로 생성된 예약 날짜와 시간의 URI")
+                        )
+                ));
+    }
+
+    @DisplayName("점주는 잘못된 예약 날짜와 시간 정보로 매장 예약을 생성할 수 없다.")
+    @Test
+    void createShopReservationDateTimeWithInvalidInput() throws Exception {
+        var shopId = 1L;
+        var request = new HashMap<>();
+        request.put("localDate", "asdf");
+        request.put("localTime", "asdf");
+
+        mockMvc.perform(post("/api/owner/v1/shops/{shopId}/reservation/date", shopId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.statusCode").value("400"))
+                .andExpect(jsonPath("$.data.title").value("HttpMessageNotReadableException"))
+                .andDo(document("owner-shop-reservation-date-create-invalid",
+                        pathParameters(
+                                parameterWithName("shopId").description("매장 id")
+                        ),
+                        requestFields(
+                                fieldWithPath("localDate").type(JsonFieldType.STRING).description("예약 날짜").optional(),
+                                fieldWithPath("localTime").type(JsonFieldType.STRING).description("예약 시간").optional()
+                        ),
+                        responseFields(
+                                fieldWithPath("statusCode").type(JsonFieldType.NUMBER).description("상태 코드"),
+                                fieldWithPath("data.title").type(JsonFieldType.STRING).description("타이틀"),
+                                fieldWithPath("data.status").type(JsonFieldType.NUMBER).description("상태 코드"),
+                                fieldWithPath("data.detail").type(JsonFieldType.STRING).description("상세 설명"),
+                                fieldWithPath("data.instance").type(JsonFieldType.STRING).description("인스턴스 URI"),
+                                fieldWithPath("data.type").type(JsonFieldType.STRING).description("타입"),
                                 fieldWithPath("serverDateTime").type(JsonFieldType.STRING).description("서버 시간")
                         )
                 ));
