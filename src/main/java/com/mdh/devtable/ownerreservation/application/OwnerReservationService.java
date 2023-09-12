532328261e5c876749cd9c70af646c1e2ba4c68d
joyfulviper <kimsk3113@naver.com>
2023-09-13 04:35:56 +0900

    [MDH-158] feat & test : 점주가 예약가능한 시간을 등록하는 기능 구현 및 테스트

--- src/main/java/com/mdh/devtable/ownerreservation/application/OwnerReservationService.java
+++ src/main/java/com/mdh/devtable/ownerreservation/application/OwnerReservationService.java
@@ -3,7 +3,9 @@ package com.mdh.devtable.ownerreservation.application;
 import com.mdh.devtable.ownerreservation.infra.persistence.OwnerReservationRepository;
 import com.mdh.devtable.ownerreservation.presentation.dto.SeatCreateRequest;
 import com.mdh.devtable.ownerreservation.presentation.dto.ShopReservationCreateRequest;
+import com.mdh.devtable.ownerreservation.presentation.dto.ShopReservationDateTimeCreateRequest;
 import com.mdh.devtable.reservation.domain.Seat;
+import com.mdh.devtable.reservation.domain.ShopReservationDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,5 +31,16 @@ public class OwnerReservationService {
         var seat = new Seat(shopReservation, seatCreateRequest.seatType());
 
         return ownerReservationRepository.saveSeat(seat);
+    }
+
+    @Transactional
+    public Long createShopReservationDateTime(Long shopId, ShopReservationDateTimeCreateRequest shopReservationDateTimeCreateRequest) {
+        var shopReservation = ownerReservationRepository.findShopReservationByShopId(shopId)
+                .orElseThrow(() -> new NoSuchElementException("해당 ID의 매장의 예약정보가 존재하지 않습니다:" + shopId));
+        var shopReservationDateTime = new ShopReservationDateTime(shopReservation,
+                shopReservationDateTimeCreateRequest.localDate(),
+                shopReservationDateTimeCreateRequest.localTime());
+
+        return ownerReservationRepository.saveShopReservationDateTime(shopReservationDateTime);
     }
 }
