2b8491d9a179cbdc186d8b9a9c97947488bbd3ab
joyfulviper <kimsk3113@naver.com>
2023-09-13 04:06:34 +0900

    [MDH-159] feat & test : 점주가 매장의 좌석을 등록하는 기능 구현 및 테스트

--- src/main/java/com/mdh/devtable/ownerreservation/application/OwnerReservationService.java
+++ src/main/java/com/mdh/devtable/ownerreservation/application/OwnerReservationService.java
@@ -1,10 +1,14 @@
 package com.mdh.devtable.ownerreservation.application;
 
 import com.mdh.devtable.ownerreservation.infra.persistence.OwnerReservationRepository;
+import com.mdh.devtable.ownerreservation.presentation.dto.SeatCreateRequest;
 import com.mdh.devtable.ownerreservation.presentation.dto.ShopReservationCreateRequest;
+import com.mdh.devtable.reservation.domain.Seat;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
 
 @RequiredArgsConstructor
 @Service
@@ -16,5 +20,14 @@ public class OwnerReservationService {
     public Long createShopReservation(Long shopId, ShopReservationCreateRequest shopReservationCreateRequest) {
         var shopReservation = shopReservationCreateRequest.toEntity(shopId);
         return ownerReservationRepository.saveShopReservation(shopReservation);
+    }
+
+    @Transactional
+    public Long saveSeat(Long shopId, SeatCreateRequest seatCreateRequest) {
+        var shopReservation = ownerReservationRepository.findShopReservationByShopId(shopId)
+                .orElseThrow(() -> new NoSuchElementException("해당 ID의 매장의 예약정보가 존재하지 않습니다:" + shopId));
+        var seat = new Seat(shopReservation, seatCreateRequest.seatType());
+
+        return ownerReservationRepository.saveSeat(seat);
     }
 }
