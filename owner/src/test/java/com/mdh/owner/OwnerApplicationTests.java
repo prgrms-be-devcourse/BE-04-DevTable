77b53262e51d20370dab469dca874c55df7e5c13
Bombo <bombo96@chungwoon.ac.kr>
2023-09-15 02:15:37 +0900

    [MDH-Multi] feat : 멀티 모듈 적용 빌드 완료

---
+++ owner/src/test/java/com/mdh/owner/OwnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdh.owner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
