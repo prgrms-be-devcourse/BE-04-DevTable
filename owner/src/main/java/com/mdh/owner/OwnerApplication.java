5980398eb132c6266f0c163e38c19e9f355ee12b
Bombo <bombo96@chungwoon.ac.kr>
2023-09-15 02:15:37 +0900

    [MDH-Multi] feat : 멀티 모듈 적용 빌드 완료

---
+++ owner/src/main/java/com/mdh/owner/OwnerApplication.java
@@ -0,0 +1,13 @@
+package com.mdh.owner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwnerApplication.class, args);
+    }
+
+}
