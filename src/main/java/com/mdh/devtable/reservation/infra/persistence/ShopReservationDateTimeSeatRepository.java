306c81a02ad312428e6abc7277bea01b830ef16b
joyfulviper <kimsk3113@naver.com>
2023-09-14 14:31:39 +0900

    [MDH-105] refactor : 매장의 동일한 날짜, 동일한 좌석에 예약하지 못하도록 유니크 제약조건 추가, 서비스레이어 검증 로직 추가

--- src/main/java/com/mdh/devtable/reservation/infra/persistence/ShopReservationDateTimeSeatRepository.java
+++ src/main/java/com/mdh/devtable/reservation/infra/persistence/ShopReservationDateTimeSeatRepository.java
@@ -3,5 +3,8 @@ package com.mdh.devtable.reservation.infra.persistence;
 import com.mdh.devtable.reservation.domain.ShopReservationDateTimeSeat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ShopReservationDateTimeSeatRepository extends JpaRepository<ShopReservationDateTimeSeat, Long> {
+    Optional<ShopReservationDateTimeSeat> findByShopReservationDateTimeIdAndSeatId(Long shopReservationDateTimeId, Long seatId);
 }
