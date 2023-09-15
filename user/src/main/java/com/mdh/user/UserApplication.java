9b9d7b0e91b9acb8f072664abfda1b13c1ef7072
Bombo <bombo96@chungwoon.ac.kr>
2023-09-15 23:39:42 +0900

    fix : 멀티 모듈에서 UserApplication의 이름이 잘못되어있는 것을 해결

--- user/src/main/java/com/mdh/user/UserApplication.java
+++ user/src/main/java/com/mdh/user/UserApplication.java
@@ -1,6 +1,5 @@
 package com.mdh.user;
 
-import com.mdh.common.CommonApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CommonApplication.class, args);
+        SpringApplication.run(UserApplication.class, args);
     }
 
 }
