06d89d0f4c0b3800886e9753d04d4f66ba5f852b
joyfulviper <kimsk3113@naver.com>
2023-09-17 06:04:29 +0900

    [MDH-107] feat & test : 알림기능 구현(slack) 및 테스트

---
+++ alarm/src/main/java/com/mdh/alarm/common/api/SlackClient.java
@@ -0,0 +1,30 @@
+package com.mdh.alarm.common.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.openfeign.FallbackFactory;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(
+        name = "${slack-client.name}",
+        url =  "${slack-client.domain-url}",
+        fallbackFactory = SlackClient.SlackFallback.class
+)
+public interface SlackClient {
+
+    @PostMapping
+    String requestAlert(@RequestBody SlackAlertRequest request);
+
+    @Slf4j
+    @Component
+    class SlackFallback implements FallbackFactory<SlackClient> {
+
+        @Override
+        public SlackClient create(Throwable cause) {
+            log.warn("슬랙 api 응답 오류: {}", cause.getMessage());
+            return (request) -> "load failed";
+        }
+    }
+}
