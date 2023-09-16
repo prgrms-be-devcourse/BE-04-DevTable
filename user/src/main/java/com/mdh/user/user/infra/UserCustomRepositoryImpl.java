350740f6e55706aba8823c7618cf6845944be779
Bombo <bombo96@chungwoon.ac.kr>
2023-09-16 23:32:09 +0900

    [MDH-189] fix : UserCustomRepositoryImpl 파일이 누락된 부분을 수정

--- user/src/main/java/com/mdh/user/user/infra/UserCustomRepositoryImpl.java
+++ user/src/main/java/com/mdh/user/user/infra/UserCustomRepositoryImpl.java
@@ -1,2 +1,26 @@
-package com.mdh.user.user.infra;public class UserCustomRepositoryImpl {
+package com.mdh.user.user.infra;
+
+import com.mdh.common.user.domain.Role;
+import com.mdh.common.user.domain.User;
+import com.mdh.common.user.persistence.UserCustomRepository;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import static com.mdh.common.user.domain.QUser.user;
+
+@RequiredArgsConstructor
+public class UserCustomRepositoryImpl implements UserCustomRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<User> findByRole(Role role) {
+        return jpaQueryFactory
+                .select(user)
+                .from(user)
+                .where(user.role.eq(role))
+                .fetch();
+    }
 }
