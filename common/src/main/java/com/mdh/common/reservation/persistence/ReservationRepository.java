f6b352709c32794eff0230e826a78f87068c1b76
joyfulviper <kimsk3113@naver.com>
2023-09-19 00:39:22 +0900

    [MDH-90] feat & test : 예약 등록시 알림을 하는 기능 추가

--- common/src/main/java/com/mdh/common/reservation/persistence/ReservationRepository.java
+++ common/src/main/java/com/mdh/common/reservation/persistence/ReservationRepository.java
@@ -3,12 +3,14 @@ package com.mdh.common.reservation.persistence;
 import com.mdh.common.reservation.domain.Reservation;
 import com.mdh.common.reservation.domain.ReservationStatus;
 import com.mdh.common.reservation.persistence.dto.OwnerShopReservationInfoResponse;
+import com.mdh.common.reservation.persistence.dto.ReservationAlarmInfo;
 import com.mdh.common.reservation.persistence.dto.ReservationQueryDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
@@ -37,4 +39,22 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
             WHERE sp.userId = :userId AND r.reservationStatus = :reservationStatus
             """)
     List<OwnerShopReservationInfoResponse> findAllReservationsByOwnerIdAndStatus(@Param("userId") Long ownerId, @Param("reservationStatus") ReservationStatus reservationStatus);
+
+    @Query(
+            """
+            SELECT new com.mdh.common.reservation.persistence.dto.ReservationAlarmInfo(
+            r.userId,
+            s.name,
+            srdt.reservationDate,
+            srdt.reservationTime,
+            r.personCount,
+            s.shopDetails.phoneNumber,
+            s.shopDetails.info
+            )
+            FROM Reservation r
+            JOIN Shop s ON s.id = r.shopReservation.shopId
+            JOIN ShopReservationDateTime srdt ON srdt.shopReservation.shopId = s.id
+            WHERE r.id = :reservationId
+            """)
+    Optional<ReservationAlarmInfo> findReservationAlarmInfoById(@Param("reservationId") Long reservationId);
 }
