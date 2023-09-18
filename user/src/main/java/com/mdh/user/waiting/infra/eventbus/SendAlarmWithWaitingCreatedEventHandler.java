f553303c665a63c0e2ce664c0d7b0cf7b5b48491
joyfulviper <kimsk3113@naver.com>
2023-09-18 17:31:25 +0900

    [MDH-87] feat & test : 웨이팅 등록 알림 기능 추가 및 테스트

---
+++ user/src/main/java/com/mdh/user/waiting/infra/eventbus/SendAlarmWithWaitingCreatedEventHandler.java
@@ -0,0 +1,42 @@
+package com.mdh.user.waiting.infra.eventbus;
+
+import com.mdh.common.waiting.persistence.WaitingRepository;
+import com.mdh.user.global.message.AlarmMessage;
+import com.mdh.user.waiting.application.event.WaitingCreatedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import java.util.NoSuchElementException;
+
+@RequiredArgsConstructor
+@Component
+public class SendAlarmWithWaitingCreatedEventHandler {
+
+    @Value("${spring.data.redis.topic.alarm}")
+    private String topic;
+
+    private final StringRedisTemplate redisTemplate;
+    private final WaitingRepository waitingRepository;
+
+    @Async
+    @Transactional(readOnly = true)
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void sendAlarmAfterWaitingCreatedEvent(WaitingCreatedEvent event) {
+        var waitingId = event.waiting().getId();
+        var alarmInfo = waitingRepository.findWaitingAlarmInfoById(waitingId)
+                .orElseThrow(() -> new NoSuchElementException("존재하는 웨이팅 정보가 없습니다: " + waitingId));
+
+        var message = new AlarmMessage(String.valueOf(alarmInfo.userId()),
+                alarmInfo.shopName(),
+                alarmInfo.toString());
+        redisTemplate.convertAndSend(topic, message);
+    }
+
+}
