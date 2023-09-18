f965e8d3e1204b287e6e33af5415099658175ad7
Bombo <bombo96@chungwoon.ac.kr>
2023-09-19 01:50:28 +0900

    [MDH-74] feat : Shop에 최소, 최대 가격 필드를 추가하고 커스텀 구현체의 위치를 변경

--- user/src/test/java/com/mdh/user/UserApplicationTests.java
+++ user/src/test/java/com/mdh/user/UserApplicationTests.java
@@ -2,13 +2,9 @@ package com.mdh.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
-@ComponentScan({
-        "com.mdh.common"
-})
-class CommonApplicationTests {
+class UserApplicationTests {
 
     @Test
     void contextLoads() {
