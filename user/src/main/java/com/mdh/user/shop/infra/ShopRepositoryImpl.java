ebcde607124661583d36f3381574223617790775
Bombo <bombo96@chungwoon.ac.kr>
2023-09-18 22:00:15 +0900

    [MDH-74] feat : 웨이팅이 가능한 매장을 동적 조회하는 기능 구현

---
+++ user/src/main/java/com/mdh/user/shop/infra/ShopRepositoryImpl.java
@@ -0,0 +1,139 @@
+package com.mdh.user.shop.infra;
+
+import com.mdh.common.shop.domain.ShopType;
+import com.mdh.common.shop.persistence.ShopRepositoryCustom;
+import com.mdh.common.shop.persistence.dto.ShopQueryDto;
+import com.mdh.common.waiting.domain.ShopWaitingStatus;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.MultiValueMap;
+
+import java.util.Collections;
+import java.util.List;
+
+import static com.mdh.common.shop.domain.QRegion.region;
+import static com.mdh.common.shop.domain.QShop.shop;
+import static com.mdh.common.waiting.domain.QShopWaiting.shopWaiting;
+import static com.mdh.user.shop.infra.ShopSearchCondParam.*;
+import static com.mdh.user.shop.infra.ShopSearchSortParam.PRICE_ASC;
+
+@Repository
+@RequiredArgsConstructor
+public class ShopRepositoryImpl implements ShopRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<ShopQueryDto> searchShopCondition(final MultiValueMap<String, String> cond, Pageable pageable) {
+
+        return jpaQueryFactory.select(Projections.constructor(ShopQueryDto.class,
+                        shop.id,
+                        shop.name,
+                        shop.shopType,
+                        shop.shopPrice.lunchMinPrice,
+                        shop.shopPrice.lunchMaxPrice,
+                        shop.shopPrice.dinnerMinPrice,
+                        shop.shopPrice.dinnerMaxPrice,
+                        shop.region.city,
+                        shop.region.district))
+                .from(shop)
+                .join(shopWaiting)
+                .on(shop.id.eq(shopWaiting.shopId))
+                .join(shop.region, region)
+                .where(shopQueryDynamicCond(cond))
+                .orderBy(getOrderSpecifier(cond.getOrDefault(SORT.getParamName(), Collections.emptyList())))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+
+    @Override
+    public JPAQuery<Long> searchShopConditionCount(MultiValueMap<String, String> cond) {
+        return jpaQueryFactory.select(shop.count())
+                .from(shop)
+                .join(shopWaiting)
+                .on(shop.id.eq(shopWaiting.shopId))
+                .join(shop.region, region)
+                .where(shopQueryDynamicCond(cond));
+    }
+
+    private BooleanBuilder shopQueryDynamicCond(final MultiValueMap<String, String> cond) {
+        var booleanBuilder = new BooleanBuilder();
+
+        return booleanBuilder.and(shopNameContains(cond.getOrDefault(NAME.getParamName(), Collections.emptyList())))
+                .and(shopTypeEq(cond.getOrDefault(SHOP_TYPE.getParamName(), Collections.emptyList())))
+                .and(shopRegionContains(cond.getOrDefault(REGION.getParamName(), Collections.emptyList())))
+                .and(shopPriceEq(
+                        cond.getOrDefault(MIN_PRICE.getParamName(), Collections.emptyList()),
+                        cond.getOrDefault(MAX_PRICE.getParamName(), Collections.emptyList())))
+                .and(isPossibleWaiting());
+
+    }
+
+    private BooleanExpression shopNameContains(List<String> shopName) {
+        return !shopName.isEmpty() ? shop.name.eq(shopName.get(0)) : null;
+    }
+
+    private BooleanBuilder shopTypeEq(List<String> shopTypes) {
+        var booleanBuilder = new BooleanBuilder();
+
+        for (String inputShopType : shopTypes) {
+            var shopType = ShopType.valueOf(inputShopType);
+            booleanBuilder.or(shop.shopType.eq(shopType));
+        }
+
+        return booleanBuilder;
+    }
+
+    private BooleanExpression shopRegionContains(List<String> regions) {
+        return !regions.isEmpty() ? shop.region.city.contains(regions.get(0))
+                .or(shop.region.district.contains(regions.get(0))) : null;
+    }
+
+    private BooleanExpression shopPriceEq(List<String> shopMinPrices, List<String> shopMaxPrices) {
+        if (shopMinPrices.isEmpty() && shopMaxPrices.isEmpty()) {
+            return null;
+        }
+
+        if (!shopMinPrices.isEmpty() && shopMaxPrices.isEmpty()) {
+            return shop.shopPrice.lunchMinPrice.goe(Integer.parseInt(shopMinPrices.get(0)))
+                    .or(shop.shopPrice.dinnerMinPrice.goe(Integer.parseInt(shopMinPrices.get(0))));
+        }
+        if (shopMinPrices.isEmpty() && !shopMaxPrices.isEmpty()) {
+            return shop.shopPrice.lunchMaxPrice.loe(Integer.parseInt(shopMaxPrices.get(0)))
+                    .or(shop.shopPrice.dinnerMaxPrice.loe(Integer.parseInt(shopMaxPrices.get(0))));
+        }
+
+        return shopPriceBetween(shopMinPrices, shopMaxPrices);
+
+    }
+
+    private BooleanExpression shopPriceBetween(List<String> shopMinPrices, List<String> shopMaxPrices) {
+        Integer minPrice = Integer.parseInt(shopMinPrices.get(0));
+        Integer maxPrice = Integer.parseInt(shopMaxPrices.get(0));
+
+        return shop.shopPrice.lunchMinPrice.goe(minPrice).and(shop.shopPrice.lunchMaxPrice.loe(maxPrice))
+                .or(shop.shopPrice.lunchMinPrice.goe(minPrice).and(shop.shopPrice.dinnerMaxPrice.loe(maxPrice)))
+                .or(shop.shopPrice.dinnerMinPrice.goe(minPrice).and(shop.shopPrice.lunchMaxPrice.loe(maxPrice)))
+                .or(shop.shopPrice.dinnerMinPrice.goe(minPrice).and(shop.shopPrice.dinnerMaxPrice.loe(maxPrice)));
+    }
+
+    private BooleanExpression isPossibleWaiting() {
+        return shopWaiting.shopWaitingStatus.eq(ShopWaitingStatus.OPEN);
+    }
+
+    private OrderSpecifier<?> getOrderSpecifier(List<String> sorts) {
+        if (sorts.get(0).equals(PRICE_ASC.getSortParam())) {
+            return shop.shopPrice.lunchMaxPrice.asc();
+        }
+
+        return shop.shopPrice.lunchMaxPrice.desc();
+    }
+}
