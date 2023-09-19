c922555650e3fbdad7b25b81525652b00ca013c1
joyfulviper <kimsk3113@naver.com>
2023-09-19 18:37:21 +0900

    [MDH-134] feat & test : 매장 상세 정보 엔드포인 및 rest docs 작성

--- user/src/main/java/com/mdh/user/shop/presentation/ShopController.java
+++ user/src/main/java/com/mdh/user/shop/presentation/ShopController.java
@@ -2,16 +2,14 @@ package com.mdh.user.shop.presentation;
 
 import com.mdh.user.global.ApiResponse;
 import com.mdh.user.shop.application.ShopService;
+import com.mdh.user.shop.application.dto.ShopDetailInfoResponse;
 import com.mdh.user.shop.application.dto.ShopResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -27,5 +25,12 @@ public class ShopController {
 
         var result = shopService.findByConditionWithWaiting(multiParams, pageable);
         return new ResponseEntity<>(ApiResponse.ok(result), HttpStatus.OK);
+    }
+
+    @GetMapping("/{shopId}")
+    public ResponseEntity<ApiResponse<ShopDetailInfoResponse>> findShopDetailsById(@PathVariable("shopId") Long shopId) {
+        var result = shopService.findShopDetailsById(shopId);
+
+        return ResponseEntity.ok(ApiResponse.ok(result));
     }
 }
