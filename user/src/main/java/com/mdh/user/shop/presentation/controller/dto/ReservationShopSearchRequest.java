8b049b4c94469923fcb5b99e15c7a6e4ff2b19c6
heenahan <mby1997s@gmail.com>
2023-09-19 14:24:02 +0900

    [MDH-78] feat : 예약 매장 필터 조회 api 구현

---
+++ user/src/main/java/com/mdh/user/shop/presentation/controller/dto/ReservationShopSearchRequest.java
@@ -0,0 +1,33 @@
+package com.mdh.user.shop.presentation.controller.dto;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Map;
+
+public record ReservationShopSearchRequest(
+
+        @JsonSerialize(using = LocalDateSerializer.class)
+        @JsonDeserialize(using = LocalDateDeserializer.class)
+        LocalDate reservationDate,
+
+        @JsonSerialize(using = LocalTimeSerializer.class)
+        @JsonDeserialize(using = LocalTimeDeserializer.class)
+        LocalTime reservationTime,
+        Integer personCount,
+        String region,
+        Integer minPrice,
+        Integer maxPrice
+) {
+    public static ReservationShopSearchRequest of(Map<String, String> param) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.convertValue(param, ReservationShopSearchRequest.class);
+    }
+}
