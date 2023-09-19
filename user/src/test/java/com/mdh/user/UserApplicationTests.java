e14107f56694a5a1fb85d7f722e0ec7dcd7bd59f
joyfulviper <kimsk3113@naver.com>
2023-09-19 14:23:56 +0900

    [MDH-107] fix : 유저 contextLoads 수정

--- user/src/test/java/com/mdh/user/UserApplicationTests.java
+++ user/src/test/java/com/mdh/user/UserApplicationTests.java
@@ -5,10 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
-@ComponentScan({
-        "com.mdh.common"
-})
-class CommonApplicationTests {
+class UserApplicationTests {
 
     @Test
     void contextLoads() {
