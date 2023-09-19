cd1c68632e70bcb7ef0d263f8bbbc5e4cace5bce
heenahan <mby1997s@gmail.com>
2023-09-19 15:06:05 +0900

    [MDH-78] feat & test : 가격 필드 추가에 따른 쿼리 변경 및 테스트

--- common/src/main/java/com/mdh/common/shop/persistence/ShopRepositoryImpl.java
+++ common/src/main/java/com/mdh/common/shop/persistence/ShopRepositoryImpl.java
@@ -1,23 +1,35 @@
 package com.mdh.common.shop.persistence;
 
+import com.mdh.common.reservation.domain.SeatStatus;
+import com.mdh.common.shop.domain.Shop;
 import com.mdh.common.shop.domain.ShopType;
+import com.mdh.common.shop.persistence.dto.ReservationShopSearchQueryDto;
 import com.mdh.common.shop.persistence.dto.ShopQueryDto;
 import com.mdh.common.shop.persistence.dto.ShopSearchCondParam;
 import com.mdh.common.waiting.domain.ShopWaitingStatus;
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
-import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.*;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.MultiValueMap;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static com.mdh.common.reservation.QShopReservation.shopReservation;
+import static com.mdh.common.reservation.QShopReservationDateTime.shopReservationDateTime;
+import static com.mdh.common.reservation.QShopReservationDateTimeSeat.shopReservationDateTimeSeat;
 import static com.mdh.common.shop.domain.QRegion.region;
 import static com.mdh.common.shop.domain.QShop.shop;
 import static com.mdh.common.shop.persistence.dto.ShopSearchCondParam.*;
@@ -62,6 +74,108 @@ public class ShopRepositoryImpl implements ShopRepositoryCustom {
                 .on(shop.id.eq(shopWaiting.shopId))
                 .join(shop.region, region)
                 .where(shopQueryDynamicCond(cond));
+    }
+
+    @Override
+    public Page<ReservationShopSearchQueryDto> searchReservationShopByFilter(Pageable pageable,
+                                                                             LocalDate reservationDate,
+                                                                             LocalTime reservationTime,
+                                                                             Integer personCount,
+                                                                             String regionName,
+                                                                             Integer minPrice,
+                                                                             Integer maxPrice) {
+        var content = jpaQueryFactory
+                .select(Projections.constructor(ReservationShopSearchQueryDto.class,
+                        shop.id,
+                        shop.name,
+                        shop.description,
+                        shop.shopType,
+                        region.city,
+                        region.district,
+                        shop.shopPrice,
+                        seatStatusCaseBuilder().sum().as("availableSeatCount")
+                ))
+                .from(shopReservationDateTimeSeat)
+                .join(shopReservationDateTime)
+                .on(shopReservationDateTimeSeat.shopReservationDateTime.id.eq(shopReservationDateTime.id))
+                .where(shopReservationDateTime.reservationDate.eq(reservationDate)
+                        .and(shopReservationDateTime.reservationTime.eq(reservationTime)))
+                .join(shopReservation)
+                .on(shopReservation.shopId.eq(shopReservationDateTime.shopReservation.shopId))
+                .where(personLoeGoe(personCount))
+                .join(shop)
+                .on(shopReservation.shopId.eq(shop.id))
+                .join(region)
+                .on(shop.region.id.eq(region.id))
+                .where(regionContains(regionName))
+                .where(priceLoeGoe(minPrice, maxPrice))
+                .groupBy(shop.id) // 아이디로 group by
+                .orderBy(getOrderSpecifiers(pageable))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        var totalCount = jpaQueryFactory
+                .select(shop.countDistinct())
+                .from(shopReservationDateTimeSeat)
+                .join(shopReservationDateTime)
+                .on(shopReservationDateTimeSeat.shopReservationDateTime.id.eq(shopReservationDateTime.id))
+                .where(shopReservationDateTime.reservationDate.eq(reservationDate)
+                        .and(shopReservationDateTime.reservationTime.eq(reservationTime)))
+                .join(shopReservation)
+                .on(shopReservation.shopId.eq(shopReservationDateTime.shopReservation.shopId))
+                .where(personLoeGoe(personCount))
+                .join(shop)
+                .on(shopReservation.shopId.eq(shop.id))
+                .join(region)
+                .on(shop.region.id.eq(region.id))
+                .where(regionContains(regionName))
+                .where(priceLoeGoe(minPrice, maxPrice));
+
+        return PageableExecutionUtils.getPage(content, pageable, totalCount::fetchOne);
+    }
+
+    private OrderSpecifier[] getOrderSpecifiers(Pageable pageable) {
+        var sort = pageable.getSort();
+        var orderSpecifiers = new ArrayList<>();
+        orderSpecifiers.add(new OrderSpecifier<>(Order.DESC, Expressions.stringPath("availableSeatCount")));
+        var shopOrderSpecifiers = sort.get().map(o -> {
+            Order order = o.isAscending() ? Order.ASC : Order.DESC;
+            String property = o.getProperty();
+            PathBuilder<Shop> pathBuilder = new PathBuilder<>(Shop.class, "shop");
+            return new OrderSpecifier(order, pathBuilder.getString(property));
+        }).toList();
+        orderSpecifiers.addAll(shopOrderSpecifiers);
+        return orderSpecifiers.stream().toArray(OrderSpecifier[]::new);
+    }
+
+    private NumberExpression<Integer> seatStatusCaseBuilder() {
+        return new CaseBuilder()
+                .when(shopReservationDateTimeSeat.seatStatus.eq(SeatStatus.AVAILABLE))
+                .then(1)
+                .otherwise(0);
+    }
+
+    private BooleanExpression personLoeGoe(Integer personCount) {
+        return personCount != null ? shopReservation.minimumPerson.loe(personCount)
+                .and(shopReservation.maximumPerson.goe(personCount)) : null;
+    }
+
+    private BooleanExpression regionContains(String regionName) {
+        return regionName != null ? region.city.contains(regionName).or(region.district.contains(regionName)) : null;
+    }
+
+    private BooleanExpression priceLoeGoe(Integer minPrice, Integer maxPrice) {
+        if (minPrice == null) return maxPriceGoe(maxPrice);
+        return minPriceLoe(minPrice).and(maxPriceGoe(maxPrice));
+    }
+
+    private BooleanExpression minPriceLoe(Integer minPrice) {
+        return minPrice != null ? shop.shopPrice.shopMinPrice.loe(minPrice) : null;
+    }
+
+    private BooleanExpression maxPriceGoe(Integer maxPrice) {
+        return maxPrice != null ? shop.shopPrice.shopMaxPrice.goe(maxPrice) : null;
     }
 
     private BooleanBuilder shopQueryDynamicCond(final MultiValueMap<String, String> cond) {
