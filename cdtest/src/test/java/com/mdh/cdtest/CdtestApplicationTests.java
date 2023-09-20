5362dba2911d1389ecc1eb7552e0f26e07c0f1f7
Bombo <bombo96@chungwoon.ac.kr>
2023-09-20 14:38:19 +0900

    [MDH-191] test : cd 를 위한 임시 모듈을 만들어서 테스트

---
+++ cdtest/src/test/java/com/mdh/cdtest/CdtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdh.cdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
