cbd90af61a48d59891f9b530537d4ec71495356a
joyfulviper <kimsk3113@naver.com>
2023-09-20 14:32:24 +0900

    [MDH-107] refactor : redisConfig common 모듈로 이동, 개행 제거, feignConfig 파일 생성, reservation 서비스 코드 좌석 두개 추가 되는 부분 수정

---
+++ alarm/src/main/java/com/mdh/alarm/slack/FeignConfig.java
@@ -0,0 +1,9 @@
+package com.mdh.alarm.slack;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@EnableFeignClients
+@Configuration
+public class FeignConfig {
+}
--- alarm/src/test/java/com/mdh/alarm/SlackFallbackTest.java
+++ alarm/src/test/java/com/mdh/alarm/SlackFallbackTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.mock;
 
 import com.mdh.alarm.slack.SlackAlertRequest;
 import com.mdh.alarm.slack.SlackClient;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -19,8 +20,7 @@ class SlackFallbackTest {
     @Mock
     private Throwable mockThrowable;
 
-
-
+    @DisplayName("슬랙 api가 예외를 발생하면 fallback에 작성해놓은 메소드가 호출된다.")
     @Test
     void shouldReturnLoadFailedMessage() {
         // Given
