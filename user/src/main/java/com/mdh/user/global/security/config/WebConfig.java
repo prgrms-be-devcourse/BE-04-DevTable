79f914f7654d3772c1a0bb4917d120a3807e6c22
joyfulviper <kimsk3113@naver.com>
2023-09-20 22:33:17 +0900

    [MDH-196] feat & refactor : 회원 로그인, 회원가입 기능 추가(기존의 회원가입 변경)

---
+++ user/src/main/java/com/mdh/user/global/security/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.mdh.user.global.security.config;
+
+import com.mdh.user.global.security.session.CurrentUserHandlerMethodArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Profile("!test")
+@Configuration
+@EnableWebMvc
+@EnableRedisHttpSession
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new CurrentUserHandlerMethodArgumentResolver());
+    }
+
+}
---
+++ user/src/main/java/com/mdh/user/global/security/session/UserInfo.java
@@ -0,0 +1,7 @@
+package com.mdh.user.global.security.session;
+
+public record UserInfo(
+        String email,
+        Long userId
+) {
+}
