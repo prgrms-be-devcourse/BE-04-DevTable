3119132cecc2fcd5836b4533ca9d9ff0a735635b
Bombo <bombo96@chungwoon.ac.kr>
2023-09-21 20:25:51 +0900

    [MDH-179] feat : 애플리케이션 Monitoring을 위한 설정 파일 추가

---
+++ common/src/main/java/com/mdh/common/global/config/MonitoringConfig.java
@@ -0,0 +1,21 @@
+package com.mdh.common.global.config;
+
+import io.micrometer.core.aop.CountedAspect;
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MonitoringConfig {
+
+    @Bean
+    public CountedAspect countedAspect(MeterRegistry meterRegistry) {
+        return new CountedAspect(meterRegistry);
+    }
+
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry meterRegistry) {
+        return new TimedAspect(meterRegistry);
+    }
+}
