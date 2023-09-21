aa189a553b446938cd0cd403244422d4d2d3faf7
Bombo <bombo96@chungwoon.ac.kr>
2023-09-21 21:47:55 +0900

    [MDH-179] refactor : MonitoringConfig 설정 파일을 test 에서 읽지 않도록 변경

--- common/src/main/java/com/mdh/common/global/config/MonitoringConfig.java
+++ common/src/main/java/com/mdh/common/global/config/MonitoringConfig.java
@@ -5,7 +5,9 @@ import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile({"default", "dev"})
 @Configuration
 public class MonitoringConfig {
 
