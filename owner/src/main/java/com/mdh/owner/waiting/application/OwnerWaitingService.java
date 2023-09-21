71daafe3f8370af2dfe17c59bdd879f491d8cf05
Bombo <bombo96@chungwoon.ac.kr>
2023-09-21 22:18:38 +0900

    [MDH-179] feat : 웨이팅 등록, 취소, 미루기에 대한 counted 추가

--- owner/src/main/java/com/mdh/owner/waiting/application/OwnerWaitingService.java
+++ owner/src/main/java/com/mdh/owner/waiting/application/OwnerWaitingService.java
@@ -1,18 +1,21 @@
 package com.mdh.owner.waiting.application;
 
 import com.mdh.common.waiting.domain.WaitingStatus;
-import com.mdh.owner.waiting.infra.persistence.OwnerWaitingRepository;
 import com.mdh.common.waiting.persistence.dto.WaitingInfoResponseForOwner;
+import com.mdh.owner.waiting.infra.persistence.OwnerWaitingRepository;
 import com.mdh.owner.waiting.presentation.dto.OwnerShopWaitingStatusChangeRequest;
 import com.mdh.owner.waiting.presentation.dto.OwnerUpdateShopWaitingInfoRequest;
 import com.mdh.owner.waiting.presentation.dto.OwnerWaitingStatusChangeRequest;
+import io.micrometer.core.annotation.Counted;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class OwnerWaitingService {
@@ -26,11 +29,14 @@ public class OwnerWaitingService {
         shopWaiting.changeShopWaitingStatus(request.shopWaitingStatus());
     }
 
+    @Counted("owner.waiting.change")
     @Transactional
     public void changeWaitingStatus(Long waitingId, OwnerWaitingStatusChangeRequest request) {
         var waiting = ownerWaitingRepository.findWaitingByWaitingId(waitingId)
                 .orElseThrow(() -> new NoSuchElementException("웨이팅 조회 결과가 없습니다."));
+
         waiting.changeWaitingStatus(request.waitingStatus());
+        log.info("웨이팅의 상태를 {}로 변경하였습니다.", request.waitingStatus().name());
     }
 
     @Transactional(readOnly = true)
