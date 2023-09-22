11fab099157f53b6a21fa1e2e3932c4afafae43e
heenahan <mby1997s@gmail.com>
2023-09-22 17:45:01 +0900

    [MDH-194] feat & test : 매장 웨이팅 라인에서 첫번째 웨이팅을 가져오는 기능 추가 및 테스트

--- common/src/main/java/com/mdh/common/waiting/persistence/WaitingLine.java
+++ common/src/main/java/com/mdh/common/waiting/persistence/WaitingLine.java
@@ -1,6 +1,7 @@
 package com.mdh.common.waiting.persistence;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public interface WaitingLine {
 
@@ -13,4 +14,8 @@ public interface WaitingLine {
     void cancel(Long shopId, Long waitingId, LocalDateTime issuedTime);
 
     void postpone(Long shopId, Long waitingId, LocalDateTime preIssuedTime, LocalDateTime issuedTime);
+
+    default Optional<Long> visit(Long shopId) {
+        throw new IllegalCallerException("해당 메서드를 호출할 수 없습니다.");
+    }
 }
--- user/src/main/java/com/mdh/user/waiting/infra/persistence/RedisWaitingLine.java
+++ user/src/main/java/com/mdh/user/waiting/infra/persistence/RedisWaitingLine.java
@@ -4,11 +4,16 @@ import com.mdh.common.waiting.persistence.WaitingLine;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Primary;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Optional;
 
 @Primary
 @Component
@@ -20,11 +25,22 @@ public class RedisWaitingLine implements WaitingLine {
 
     @Override
     public void save(Long shopId, Long waitingId, LocalDateTime issuedTime) {
-        var zSetOperations = redisTemplate.opsForZSet();
-        long issuedTimeLong = issuedTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
-        log.debug("waitingId = {}, issuedDate = {}", waitingId, issuedTimeLong);
-        // key score value
-        zSetOperations.add(String.valueOf(shopId), String.valueOf(waitingId), issuedTimeLong);
+        redisTemplate.execute(new SessionCallback<Object>() {
+            @Override
+            public <K, V> Object execute(RedisOperations<K, V> operations) throws DataAccessException {
+                try {
+                    operations.multi();
+                    var zSetOperations = redisTemplate.opsForZSet();
+                    long issuedTimeLong = issuedTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+                    log.debug("waitingId = {}, issuedDate = {}", waitingId, issuedTimeLong);
+                    // key score value
+                    zSetOperations.add(String.valueOf(shopId), String.valueOf(waitingId), issuedTimeLong);
+                } catch (Exception e) {
+                    operations.discard();
+                }
+                return operations.exec();
+            }
+        });
     }
 
     @Override
@@ -39,28 +55,59 @@ public class RedisWaitingLine implements WaitingLine {
     public long findTotalWaiting(Long shopId) {
         var zSetOperations = redisTemplate.opsForZSet();
         Long totalWaiting = zSetOperations.size(String.valueOf(shopId));
-        if (totalWaiting == null) throw new IllegalArgumentException();
+        log.info("totalWaiting = {}", totalWaiting);
+        if (totalWaiting == null) throw new IllegalArgumentException("해당 매장의 웨이팅 라인이 존재하지 않습니다. shopId : " + shopId);
         return totalWaiting;
     }
 
     @Override
     public void cancel(Long shopId, Long waitingId, LocalDateTime issuedTime) {
-        var zSetOperations = redisTemplate.opsForZSet();
-        zSetOperations.remove(String.valueOf(shopId), String.valueOf(waitingId));
+        redisTemplate.execute(new SessionCallback<Object>() {
+            @Override
+            public <K, V> Object execute(RedisOperations<K, V> operations) throws DataAccessException {
+                try {
+                    operations.multi();
+                    var zSetOperations = redisTemplate.opsForZSet();
+                    zSetOperations.remove(String.valueOf(shopId), String.valueOf(waitingId));
+                } catch (Exception e) {
+                    operations.discard();
+                }
+                return operations.exec();
+            }
+        });
     }
 
     @Override
     public void postpone(Long shopId, Long waitingId, LocalDateTime preIssuedTime, LocalDateTime issuedTime) {
-        var zSetOperations = redisTemplate.opsForZSet();
         validPostpone(shopId, waitingId, issuedTime);
-
-        long issuedTimeLong = issuedTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
-        zSetOperations.add(String.valueOf(shopId), String.valueOf(waitingId), issuedTimeLong);
+        redisTemplate.execute(new SessionCallback<Object>() {
+            @Override
+            public <K, V> Object execute(RedisOperations<K, V> operations) throws DataAccessException {
+                try {
+                    operations.multi();
+                    long issuedTimeLong = issuedTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+                    redisTemplate.opsForZSet().add(String.valueOf(shopId), String.valueOf(waitingId), issuedTimeLong);
+                } catch (Exception e) {
+                    log.debug("웨이팅을 미뤘을 때 에러가 발생했습니다. ", e);
+                    operations.discard();
+                }
+                return operations.exec();
+            }
+        });
     }
 
     private void validPostpone(Long shopId, Long waitingId, LocalDateTime issuedTime) {
         if (findRank(shopId, waitingId, issuedTime) == findTotalWaiting(shopId)) {
             throw new IllegalStateException("미루기를 수행 할 수 없는 웨이팅 입니다. " + waitingId);
         }
+    }
+
+    @Override
+    public Optional<Long> visit(Long shopId) {
+        var zSetOperations = redisTemplate.opsForZSet();
+        TypedTuple<String> first = zSetOperations.popMin(String.valueOf(shopId));
+        if (first == null) throw new IllegalCallerException("파이프라인이나 트랜잭션에서 호출 시 null을 반환합니다.");
+        String firstValue = first.getValue();
+        return Optional.of(Long.valueOf(firstValue));
     }
 }
