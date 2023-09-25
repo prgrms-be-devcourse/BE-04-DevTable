98950cd8dcadac89805876fd35236da8048dfa68
joyfulviper <kimsk3113@naver.com>
2023-09-26 00:50:29 +0900

    [MDH-206] refacotr : cors 정책 인터셉터가 아닌 security filter 기반으로 변경

--- owner/src/main/java/com/mdh/owner/global/security/config/WebConfig.java
+++ owner/src/main/java/com/mdh/owner/global/security/config/WebConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,21 +16,9 @@ import java.util.List;
 @EnableRedisHttpSession
 public class WebConfig implements WebMvcConfigurer {
 
-    private static final long MAX_AGE_SECS = 3600;
-
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new CurrentUserHandlerMethodArgumentResolver());
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH")
-                .allowedOrigins("http://localhost:3000")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(MAX_AGE_SECS);
     }
 
 }
